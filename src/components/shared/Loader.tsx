9014f4704712e1b4ec00c2739eb2a62bcc75ff50
yasir4560 <yasirsyed180@gmail.com>
2023-12-29 15:23:22 +0530

    creating a social media application

---
+++ src/components/shared/Loader.tsx
@@ -0,0 +1,14 @@
+
+
+const Loader = () => {
+  return (
+    <div className="flex-center w-full">
+      <img src="/asssts/icon/loader.svg" alt="loader" 
+      width={24}
+      height={24}
+      />
+    </div>
+  )
+}
+
+export default Loader
